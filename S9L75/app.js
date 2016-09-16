7d7309b26ac22771bc04091a44dba5a6436cb147
Joao Gouveia <jgouveia@avenuecode.com>
2016-09-16 14:37:12 -0300

    S975 to 9

---
+++ S9L75/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  console.log('get method called!');
+  res.send('<html><head></head><body><h1>Hello world</h1></body></html>');
+});
+
+app.get('/person/:id', function(req, res){
+  res.send('<html><head><link href=/assets/style.css type=text/css rel=stylesheet /></head><body><h1>Person: '+req.params.id+'</h1></body></html>');
+});
+
+app.get('/api', function(req, res){
+  res.json({firstname: 'Joao', lastname: 'Gouveia'});
+});
+
+app.listen(port);
